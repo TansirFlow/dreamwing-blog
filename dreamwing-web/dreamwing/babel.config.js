924f1327625761047eb3e195b9366b08121642da
MrTan713 <2315770989@qq.com>
2024-05-02 02:47:33 +0800

    优化代码高亮

---
+++ dreamwing-web/dreamwing/babel.config.js
@@ -0,0 +1,13 @@
+const components = require('prismjs/components');
+const allLanguages = Object.keys(components.languages).filter((item) => item !== 'meta');
+
+module.exports = {
+  plugins: [
+    [
+      'prismjs',
+      {
+        languages: allLanguages,
+      },
+    ],
+  ],
+};
